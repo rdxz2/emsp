bc577f33c8aaf749d7a5b3372f08557ea07a2f85
RD <richard.dharmawan@gmail.com>
2018-05-14 20:14:24 +0700

    test2

--- emsp/Form1.Designer.cs
+++ emsp/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,21 +41,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "testing";
             // 
-            // button1
+            // b1
             // 
-            this.button1.Location = new System.Drawing.Point(105, 129);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "tot";
-            this.button1.UseVisualStyleBackColor = true;
+            this.b1.Location = new System.Drawing.Point(105, 129);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 23);
+            this.b1.TabIndex = 1;
+            this.b1.Text = "lalalalla";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -67,7 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button b1;
     }
 }
 
